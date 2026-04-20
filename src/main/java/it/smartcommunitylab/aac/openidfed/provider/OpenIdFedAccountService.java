a0bafb0117a8308c86ce8142cc049b75a66d3dfb
Federico <federico.graziola@tndigit.it>
2026-04-20 11:19:55 +0200

    feat: make account openidfed editable in user console

--- src/main/java/it/smartcommunitylab/aac/openidfed/provider/OpenIdFedAccountService.java
+++ src/main/java/it/smartcommunitylab/aac/openidfed/provider/OpenIdFedAccountService.java
@@ -19,6 +19,7 @@ package it.smartcommunitylab.aac.openidfed.provider;
 import it.smartcommunitylab.aac.SystemKeys;
 import it.smartcommunitylab.aac.accounts.base.AbstractAccountService;
 import it.smartcommunitylab.aac.accounts.persistence.UserAccountService;
+import it.smartcommunitylab.aac.common.NoSuchUserException;
 import it.smartcommunitylab.aac.oidc.model.OIDCEditableUserAccount;
 import it.smartcommunitylab.aac.oidc.model.OIDCUserAccount;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,5 +45,42 @@ public class OpenIdFedAccountService
         String realm
     ) {
         super(authority, providerId, accountService, config, realm);
+    }
+
+    @Override
+    public OIDCEditableUserAccount getEditableAccount(String userId, String subject) throws NoSuchUserException {
+        OIDCUserAccount account = findAccount(subject);
+        if (account == null) {
+            throw new NoSuchUserException();
+        }
+
+        if (!account.getUserId().equals(userId)) {
+            throw new IllegalArgumentException("user-mismatch");
+        }
+
+        return toEditableAccount(account);
+    }
+
+    private OIDCEditableUserAccount toEditableAccount(OIDCUserAccount account) {
+        OIDCEditableUserAccount ea = new OIDCEditableUserAccount(
+                getAuthority(),
+                getProvider(),
+                getRealm(),
+                account.getUserId(),
+                account.getUuid()
+        );
+        ea.setSubject(account.getSubject());
+        ea.setUsername(account.getUsername());
+
+        ea.setCreateDate(account.getCreateDate());
+        ea.setModifiedDate(account.getModifiedDate());
+
+        ea.setEmail(account.getEmail());
+        ea.setName(account.getName());
+        ea.setGivenName(account.getGivenName());
+        ea.setFamilyName(account.getFamilyName());
+        ea.setLang(account.getLang());
+
+        return ea;
     }
 }
